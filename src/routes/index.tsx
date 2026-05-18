baf3fc34dd8d6d167fff5aeccf5924007b195ef7
vaultcrafted <vaultcrafted@gmail.com>
2026-05-19 01:11:55 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -134,7 +134,7 @@ function HomePage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
           {categories.map((c, i) => (
             <Link
               key={c.slug}
