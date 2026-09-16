d8245eda91aa35bac0315f99462b76ca41963f2e
vaultcrafted <vaultcrafted@gmail.com>
2026-09-16 16:15:35 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -146,9 +146,20 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
       <head>
+            <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3NB9VML');</script>
+<!-- End Google Tag Manager -->
         <HeadContent />
       </head>
       <body>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3NB9VML"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         {children}
         <Scripts />
       </body>
